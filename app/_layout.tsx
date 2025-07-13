62ad00c34606fbe9a9d701b4461511fad364ac21
Rishikesh Bhatt <rish1805@gmail.com>
2025-07-13 18:35:02 +0200

    Fixed white flashing on screen during page transition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,17 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{
-    headerShown: false,
-  }} />;
+  return (
+    <View style={{ flex: 1, backgroundColor: '#0f172a' }}>
+      <Stack 
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#0f172a' },
+          animation: "slide_from_right",
+        }} 
+      />
+    </View>
+  );
 }
